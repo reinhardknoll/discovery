e253de50af0b9d5d65426fab7cb607526388dcd6
Reinhard <reinhard.knoll@gmail.com>
2022-03-28 23:02:46 +1300

    Updated example for LED compass

--- microbit/src/09-led-compass/src/main.rs
+++ microbit/src/09-led-compass/src/main.rs
@@ -10,6 +10,14 @@ mod calibration;
 use crate::calibration::calc_calibration;
 use crate::calibration::calibrated_measurement;
 
+mod led;
+use led::direction_to_led;
+use led::Direction;
+
+use core::f32::consts::PI;
+use libm::atan2f;
+use libm::sqrtf;
+
 use microbit::{display::blocking::Display, hal::Timer};
 
 #[cfg(feature = "v1")]
@@ -47,6 +55,40 @@ fn main() -> ! {
         while !sensor.mag_status().unwrap().xyz_new_data {}
         let mut data = sensor.mag_data().unwrap();
         data = calibrated_measurement(data, &calibration);
-        rprintln!("x: {}, y: {}, z: {}", data.x, data.y, data.z);
+
+        // use libm's atan2f since this isn't in core yet
+        let theta = atan2f(data.y as f32, data.x as f32);
+
+        // Figure out the direction based on theta
+        let dir = if theta < -7. * PI / 8. {
+            Direction::West
+        } else if theta < -5. * PI / 8. {
+            Direction::SouthWest
+        } else if theta < -3. * PI / 8. {
+            Direction::South
+        } else if theta < -PI / 8. {
+            Direction::SouthEast
+        } else if theta < PI / 8. {
+            Direction::East
+        } else if theta < 3. * PI / 8. {
+            Direction::NorthEast
+        } else if theta < 5. * PI / 8. {
+            Direction::North
+        } else if theta < 7. * PI / 8. {
+            Direction::NorthWest
+        } else {
+            Direction::West
+        };
+
+        // use the led module to turn the direction into an LED arrow
+        // and the led display functions from chapter 5 to display the
+        // arrow
+        display.show(&mut timer, direction_to_led(dir), 100);
+
+        let x = data.x as f32;
+        let y = data.y as f32;
+        let z = data.z as f32;
+        let magnitude = sqrtf(x * x + y * y + z * z);
+        rprintln!("{} nT, {} mG", magnitude, magnitude / 100.0);
     }
 }
